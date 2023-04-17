cc4d003eae252c63c7556d4158de3865d8969448
hiraaziz <hiraaziz1998@gmail.com>
2023-04-17 14:56:07 +0500

    Update route.ts

--- bookstore/app/api/books/route.ts
+++ bookstore/app/api/books/route.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 import { connectdatabase } from "../../../components/databaseConn";
 
 export async function GET(request: Request) {
-  const urls = request.url;
-  const url = new URL(urls);
-  const type = url.searchParams?.get("type");
-  const limit = url.searchParams?.get("limit");
+  const urls: string = request.url;
+  const url: URL = new URL(urls);
+  const type: string | null = url.searchParams?.get("type");
+  const limit: string | null = url.searchParams?.get("limit");
 
   let sql = connectdatabase();
 
@@ -17,17 +17,20 @@ export async function GET(request: Request) {
     return NextResponse.json({ result });
   }
   if (type) {
-    const result = await sql.unsafe("select * from book where type = $1", [
-      type,
-    ]);
+    const result: book[] = await sql.unsafe(
+      "select * from book where type = $1",
+      [type]
+    );
     return NextResponse.json({ result });
   }
   if (limit) {
-    const result = await sql.unsafe("select * from book limit $1", [limit]);
+    const result: book[] = await sql.unsafe("select * from book limit $1", [
+      limit,
+    ]);
     return NextResponse.json({ result });
   }
   try {
-    const result = await sql.unsafe("select * from book");
+    const result: book[] = await sql.unsafe("select * from book");
     return NextResponse.json({ result });
   } catch (error: any) {
     return NextResponse.json({ message: "bad request" });
